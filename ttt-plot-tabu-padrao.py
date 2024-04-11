79c68880322e0cdc7252c2138b2d200314b1eb02
Pedro Machado Martins Leao <ra243183@beach.lab.ic.unicamp.br>
2024-04-11 17:23:37 -0300

    Add tabu plots

--- ttt-plot-tabu-padrao.py
+++ ttt-plot-tabu-padrao.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 ttt_tabu_padrao_data = []
 file = open("ts_padrao_final.txt", "r")
@@ -20,17 +21,53 @@ for i in range(len(ttt_tabu_padrao_data)):
     if(ttt_tabu_padrao_data[i][-1][0] == -1):
         ttt_tabu_padrao_data[i].pop()
     
-    if(ttt_tabu_padrao_data[i][-1][1] < 1000):
-        continue
+    
+    x_points.append(ttt_tabu_padrao_data[i][-1][0])
+    prob = (i - 0.5)/len(ttt_tabu_padrao_data)
+    y_points.append(prob) # append no tempo de execução da ultima iteração
+
+x_points.sort()
+
+
+plt.plot(x_points, y_points, "+")
+
+
+plt.savefig("ttt-tabu-padrao.png")
+plt.clf()
+    
+    
+ttt_tabu_diver_restart_data = []
+file = open("ts_diver_restart.txt", "r")
+while(True):
+    line = file.readline().strip()
+    if(not line):
+        break
+    line_list = line.split()
+    if(line_list[0] == "Iteration"):
+        ttt_tabu_diver_restart_data.append([])
     else:
-        x_points.append(i)
-        y_points.append(ttt_tabu_padrao_data[i][-1][0]) # append no tempo de execução da ultima iteração
+        line_list = [int(x) for x in line_list]
+        ttt_tabu_diver_restart_data[-1].append(line_list)
 
-y_points.sort()
+x_points = []
+y_points = []
 
-plt.plot(x_points, y_points)
-plt.show()
+for i in range(len(ttt_tabu_diver_restart_data)):
+    if(ttt_tabu_diver_restart_data[i][-1][0] == -1):
+        ttt_tabu_diver_restart_data[i].pop()
+    
+    
+    x_points.append(ttt_tabu_diver_restart_data[i][-1][0])
+    prob = (i - 0.5)/len(ttt_tabu_diver_restart_data)
+    y_points.append(prob) # append no tempo de execução da ultima iteração
 
+x_points.sort()
+
+
+plt.plot(x_points, y_points, "+")
+
+
+plt.savefig("ttt-tabu-diver-restart.png")
 
 
     
